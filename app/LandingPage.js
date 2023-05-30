be23a7b54b92d37517232a9a48b477e9f7d24ff4
Olayiwola Olanrewaju <larry_coal@outlook.com>
2023-05-30 14:53:54 -0400

    team and how to page

--- app/LandingPage.js
+++ app/LandingPage.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./home.module.css";
+import {
+  FaShareAltSquare,
+  FaLocationArrow,
+  FaStaylinked,
+} from "react-icons/fa";
+import lanreBitmoji from "../assets/images/lanre_bitmoji.png";
 import { BsLink, BsFillCalendar2CheckFill, BsShareFill } from "react-icons/bs";
 import { CiLocationOn } from "react-icons/ci";
 import { IoIosPeople } from "react-icons/io";
@@ -22,16 +29,16 @@ const LandingPage = () => {
           <div className={styles.right}>
             <div className={styles.logo_container}>
               <span>
-                <BsLink />
+                <FaLocationArrow />
               </span>
               <span>
                 <BsFillCalendar2CheckFill />
               </span>
               <span>
-                <CiLocationOn />
+                <FaStaylinked />
               </span>
               <span>
-                <BsShareFill />
+                <FaShareAltSquare />
               </span>
               <span>
                 <IoIosPeople />
@@ -43,8 +50,65 @@ const LandingPage = () => {
       </section>
       <section className={styles.howto}>
         <div className={styles.container}>
-            <h2>Getting Started</h2>
-            <div></div>
+          <h2>How it works</h2>
+          <div className={styles.instructions}>
+            <ul>
+              <li>
+                <h3>Create an account for free</h3>
+                <p>
+                  Dolore laboris non ex irure labore sit laboris. Elit et sit
+                  quis eiusmod dolore id voluptate reprehenderit consequat
+                  incididunt dolor nulla nulla. Tempor nostrud ad minim est
+                  minim nostrud irure commodo Lorem veniam et cillum.
+                </p>
+              </li>
+              <li>
+                <h3>Create an account for free</h3>
+                <p>
+                  Dolore laboris non ex irure labore sit laboris. Elit et sit
+                  quis eiusmod dolore id voluptate reprehenderit consequat
+                  incididunt dolor nulla nulla. Tempor nostrud ad minim est
+                  minim nostrud irure commodo Lorem veniam et cillum.
+                </p>
+              </li>
+              <li>
+                <h3>Create an account for free</h3>
+                <p>
+                  Dolore laboris non ex irure labore sit laboris. Elit et sit
+                  quis eiusmod dolore id voluptate reprehenderit consequat
+                  incididunt dolor nulla nulla. Tempor nostrud ad minim est
+                  minim nostrud irure commodo Lorem veniam et cillum.
+                </p>
+              </li>
+              <li>
+                <h3>Create an account for free</h3>
+                <p>
+                  Dolore laboris non ex irure labore sit laboris. Elit et sit
+                  quis eiusmod dolore id voluptate reprehenderit consequat
+                  incididunt dolor nulla nulla. Tempor nostrud ad minim est
+                  minim nostrud irure commodo Lorem veniam et cillum.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className={styles.team}>
+        <h2>The team</h2>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <Image src={lanreBitmoji} width="400" height="400" />
+          </div>
+          <div className={styles.right}>
+            <h3>Hi! My name is Olanrewaju</h3>
+            <p>
+              Laborum eiusmod est ut minim fugiat laboris excepteur ea fugiat et
+              est cupidatat. Et incididunt cupidatat eiusmod ut non do minim
+              duis officia id sunt magna voluptate ad. Duis esse Lorem officia
+              do. duis officia id sunt magna voluptate ad. Duis esse Lorem
+              officia do.
+            </p>
+          </div>
         </div>
       </section>
     </div>
